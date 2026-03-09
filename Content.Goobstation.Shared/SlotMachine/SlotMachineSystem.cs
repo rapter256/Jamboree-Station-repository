60e772fb5f1ad93a722a05dba352bf3cb7ceb7a9
JamboreeBot <JamboreeBot@proton.me>
2026-03-09 03:46:38 +0000

    chore: Automatically update REUSE headers

--- Content.Goobstation.Shared/SlotMachine/SlotMachineSystem.cs
+++ Content.Goobstation.Shared/SlotMachine/SlotMachineSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Goob Station Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System.Linq;
 using Content.Shared.DoAfter;
 using Content.Shared.Popups;
