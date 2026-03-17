7f5cdf6cea3b826b61652930d29b25e1bbeef5e9
JamboreeBot <JamboreeBot@proton.me>
2026-03-17 02:28:28 +0000

    chore: Automatically update REUSE headers

--- Content.Shared/KillTome/KillTomeComponent.cs
+++ Content.Shared/KillTome/KillTomeComponent.cs
@@ -1,4 +1,8 @@
-﻿using Content.Goobstation.Maths.FixedPoint;
+// SPDX-FileCopyrightText: 2026 Space Station 14 Contributors
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Goobstation.Maths.FixedPoint;
 using Content.Shared.Damage;
 using Content.Shared.Damage.Prototypes;
 using Robust.Shared.GameStates;
